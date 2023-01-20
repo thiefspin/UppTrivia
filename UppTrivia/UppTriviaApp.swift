f0890e8e63473d7d487bb6b3b27f9a8cf3da9cf6
michaelhoward <michael.howard.h@gmail.com>
2023-01-20 09:55:37 +0200

    Initial Commit

---
+++ UppTrivia/UppTriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  UppTriviaApp.swift
+//  UppTrivia
+//
+//  Created by Michael Howard on 2023/01/20.
+//
+
+import SwiftUI
+
+@main
+struct UppTriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
